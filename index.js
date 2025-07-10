8ca28266e48060f04a743f6109811502a794583d
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-10 12:03:04 +0600

    simple express setup

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 /** @format */
 
-import React from "react";
+const express = require("express");
+const cors = require("cors");
 
-export const index = () => {
-  return <div>index</div>;
-};
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("doctor is running");
+});
+app.listen(port, () => {
+  console.log(`Car Doctor is running on port : ${port}`);
+});
