329a5bf9d9e77fbd2b9c03cf5d927e6f3289f896
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-14 02:46:24 -0600

    aded bookings

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -48,6 +49,11 @@ async function run() {
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
+    });
+
+    //bookings
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
     });
 
     // Send a ping to confirm a successful connection
