892e3b688477adec8a6aa87b0aea186ae005500e
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-14 05:36:03 -0600

    updated

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ async function run() {
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
