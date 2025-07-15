383b9fa53025be9d60110837900a16a3da03b5ce
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-14 22:59:33 -0600

    updated

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
@@ -53,7 +53,7 @@ async function run() {
 
     //bookings
 
-    app.get("/booking", async (req, res) => {
+    app.get("/bookings", async (req, res) => {
       console.log(req.query.email);
       let query = {};
       if (req.query?.email) {
