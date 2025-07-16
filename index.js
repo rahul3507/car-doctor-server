ba45089e9d1a68d6d73406e4b6ab962ffd45459b
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-16 02:23:44 -0600

    create jwt token

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       console.log(user);
-      res.send(user);
+      const token = jwt.sign(user, "secret", { expiresIN: "1h" });
+
+      res.send(token);
     });
 
     //service
