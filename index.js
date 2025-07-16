6797a4e9a1c3bd733ace5513de9ba3c6a90e44e1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-16 04:55:39 -0600

    set origin and credentials into cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const port = process.env.PORT || 5000;
 
 //middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.deuak1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
